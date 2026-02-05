6d6d50267370568ee15ecf0643d1153dd43459de
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-02-04 19:36:55 -0500

    login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'package:votacionesappg14/pages/register_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool rememberMe = false;
+
+  TextEditingController _emailController = TextEditingController();
+
+  TextEditingController _passwordController = TextEditingController();
+
+  Widget _builField({
+    required TextEditingController controller,
+    required String label,
+    required String hint,
+    bool isPassword = false,
+  }) {
+    return TextField(
+      controller: controller,
+      cursorColor: Colors.orange,
+      style: TextStyle(fontWeight: FontWeight.bold),
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.amber, width: 2),
+        ),
+      ),
+      keyboardType: isPassword
+          ? TextInputType.visiblePassword
+          : TextInputType.emailAddress,
+    );
+  }
+
+  Widget _buildRememberMeCheckbox() {
+    return Row(
+      children: [
+        Checkbox(
+          value: rememberMe,
+          onChanged: (value) {
+            setState(() {
+              rememberMe = value ?? false;
+            });
+          },
+        ),
+        Text("Recordar"),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: size.height,
+            width: size.width,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff173D31), Colors.white],
+                stops: [0.5, 0.5],
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: size.height / 2,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://images.pexels.com/photos/1128334/pexels-photo-1128334.jpeg",
+                          ),
+                          fit: BoxFit.cover,
+                          alignment: Alignment.topCenter,
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      width: double.infinity,
+                      height: size.height / 2,
+                      color: Color(0xff173D31).withOpacity(0.7),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 120,
+                            height: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.abc,
+                              size: 80,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            "Bienvenido de nuevo!",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+                height: size.height / 2,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(50)),
+                ),
+                child: Column(
+                  children: [
+                    _builField(
+                      controller: _emailController,
+                      label: "Correo",
+                      hint: "Ingresa tu correo",
+                    ),
+                    SizedBox(height: 32),
+                    _builField(
+                      controller: _passwordController,
+                      label: "Contraseña",
+                      hint: "Ingresa tu contraseña",
+                      isPassword: true,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildRememberMeCheckbox(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text("Olvidaste la contraseña?"),
+                        ),
+                      ],
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 100,
+                            vertical: 15,
+                          ),
+                          backgroundColor: Color(0xff6F9575),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        child: Text("Iniciar sesión"),
+                      ),
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("No tienes una cuenta? "),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => RegisterPage(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            "CREA UNA",
+                            style: TextStyle(
+                              color: Color(0xff173D31),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("ó inicia sesión con: "),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: CircleBorder(),
+                          ),
+                          child: Icon(Icons.g_mobiledata),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
